8d725e4e6ab867e7866083d79f6cdc3d90634272
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-06-16 10:33:37 -0500

    Creación del Content Provider
    Dentro del paquete provider crea una nueva clase para el contrato del provider llamada ContractParaGastos.java
    
    * ESTADO: Marca de un registro que indica si está siendo sincronizado o está intacto. Para referirnos a estas dos condiciones usamos las banderas ESTADO_OK y ESTADO_SYNC.
    
    * ID_REMOTA: Es necesario tener una copia de la llave primaria que tiene el registro local en la base de datos del servidor. Esto permite mantener una actualización basada en una sola referencia.
    
    * PENDIENTE_INSERCION: Cuando un gasto es añadido en la base de datos local Sqlite, debe ser marcado como “pendiente de inserción”. ¿Por qué? … Bueno, es necesario “ensuciar” este registro por si la conexión a internet falla o si la sincronización se detiene por causas externas. Así cuando se vuelva a intentar retomar la sincronización, el registro que quedó pendiente, podrá intentarse subir de nuevo y no perder los datos.

---
+++ app/src/main/java/com/example/sync/provider/ContractParaGastos.java
@@ -0,0 +1,85 @@
+package com.example.sync.provider;
+
+import android.content.UriMatcher;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+import java.net.URI;
+
+/**
+ *  Contract class entre el provider y las aplicaciones
+ */
+public class ContractParaGastos {
+
+    /**
+     * Autoridad del ContentProvider
+     */
+    public final  static String AUTHORITY = "com.example.sync";
+
+    /**
+     * Representación de la tabla a consultar
+     */
+    public  static  final String GASTO = "gasto";
+
+    /**
+     * Tipo MIME que retorna la consulta de una sola fila
+     */
+    public static final String SINGLE_MIME = "vnd.android.cursor.item/vnd."+ AUTHORITY + GASTO;
+
+    /**
+     * Tipo MIME que retorna la consulta de {@link CONTENT_URI}
+     */
+    public static final  String MULTIPLE_MIME = "vnd.android.cursor.dir/vnd." + AUTHORITY + GASTO;
+
+    /**
+     * URI del contenido principal
+     */
+    public static final Uri CONTENT_URI = Uri.parse("content://"+ AUTHORITY + "/" + GASTO);
+
+    /**
+     * Comparador de URIs de contenido
+     */
+    public static final UriMatcher uriMatcher;
+
+    /**
+     * Código para URIs de multiples registros
+     */
+    public static final int ALLROWS = 1;
+    /**
+     * Código para URIS de un solo registro
+     */
+    public static final int SINGLE_ROW = 2;
+
+
+    // Asignación de URIs
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(AUTHORITY, GASTO, ALLROWS);
+        uriMatcher.addURI(AUTHORITY, GASTO + "/#", SINGLE_ROW);
+    }
+
+    // Valores para la columna ESTADO
+    public static final int ESTADO_OK = 0;
+    public static final int ESTADO_SYNC = 1;
+
+    /**
+     * Estructura de la tabla
+     */
+    public static class Columnas implements BaseColumns {
+
+        private Columnas() {
+            // Sin instancias
+        }
+
+        public final static String MONTO = "monto";
+        public final static String ETIQUETA = "etiqueta";
+        public final static String FECHA = "fecha";
+        public final static String DESCRIPCION = "descripcion";
+
+        public static final String ESTADO = "estado";
+        public static final String ID_REMOTA = "idRemota";
+        public final static String PENDIENTE_INSERCION = "pendiente_insercion";
+
+    }
+
+}
